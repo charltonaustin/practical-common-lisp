80637018860f68c138b5e83235b6fec23898b1fe
charltonaustin <charlton.austin@gmail.com>
2025-04-06 16:37:38 -0700

    Notes from practical creating a spam filter 1

--- spam/temp.lisp
+++ spam/temp.lisp
@@ -3,10 +3,14 @@
 (defparameter *max-ham-score* .4)
 (defparameter *min-spam-score* .6)
 
+(defvar *total-spams* 0)
+(defvar *total-hams* 0)
 (defvar *feature-database* (make-hash-table :test #'equal))
 
 (defun clear-database ()
-  (setf *feature-database* (make-hash-table :test #'equal)))
+  (setf *feature-database* (make-hash-table :test #'equal))
+  (setf *total-spams* 0)
+  (setf *total-hams* 0))
 
 (defun classification (score)
   (cond
@@ -47,3 +51,40 @@
 
 (defun extract-features (text)
   (mapcar #'intern-feature (extract-words text)))
+
+
+(defmethod print-object ((object word-feature) stream)
+  (print-unreadable-object (object stream :type t)
+    (with-slots (word ham-count spam-count) object
+      (format stream "~s :hams ~d :spams ~d" word ham-count spam-count))))
+
+
+(defun train (text type)
+  (dolist (feature (extract-features text))
+    (increment-count feature type))
+  (increment-total-count type))
+
+(defun increment-count (feature type)
+  (ecase type
+    (ham (incf (ham-count feature)))
+    (spam (incf (spam-count feature)))))
+
+(defun increment-total-count (type)
+  (ecase type
+    (ham (incf *total-hams*))
+    (spam (incf *total-spams*))))
+
+(defun spam-probability (feature)
+  (with-slots (spam-count ham-count) feature
+    (let ((spam-frequency (/ spam-count (max 1 *total-spams*)))
+          (ham-frequency (/ ham-count (max 1 *total-hams*))))
+      (/ spam-frequency (+ spam-frequency ham-frequency)))))
+
+(defun bayesian-spam-probability (feature &optional
+                                            (assumed-probability 1/2)
+                                            (weight 1))
+  (let ((basic-probability (spam-probability feature))
+        (data-points (+ (spam-count feature) (ham-count feature))))
+    (/ (+ (* weight assumed-probability)
+          (* data-points basic-probability))
+       (+ weight data-points))))
