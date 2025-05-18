d15ec924c3c7ed7496d80a492c2de61545a49cad
charltonaustin <charlton.austin@gmail.com>
2025-05-18 07:34:40 -0700

    Notes from practical common lisp MP3 database pt 1

--- mp3-database/mp3-database.lisp
+++ mp3-database/mp3-database.lisp
@@ -197,3 +197,90 @@
           when (funcall comparator a-value b-value) return t
             when (funcall comparator b-value a-value) return nil
               finally (return nil)))))
+
+(defun column-matcher (column value)
+  (let ((name (name column))
+        (predicate (equality-predicate column))
+        (normalized (normalize-for-column value column)))
+    #'(lambda (row) (funcall predicate (getf row name) normalized))))
+
+(defun matching (table &rest names-and-values)
+  "Build a where function that matches rows with the given column values."
+  (let ((matchers (column-matchers (schema table) names-and-values)))
+    #'(lambda (row)
+        (every #'(lambda (matcher) (funcall matcher row)) matchers))))
+
+(defun in (column-name table)
+  (let ((test (equality-predicate (find-column column-name (schema table))))
+        (values (map 'list #'(lambda (r) (getf r column-name)) (rows table))))
+    #'(lambda (row)
+        (member (getf row column-name) values :test test))))
+
+(defmacro do-rows ((row table) &body body)
+  `(loop for ,row across (rows ,table) do ,@body))
+
+(defun map-rows (fn table)
+  (loop for row across (rows table) collect (funcall fn row)))
+
+(defun column-value (row column-name)
+  (getf row column-name))
+
+(defmacro with-column-values ((&rest vars) row &body body)
+  (once-only (row)
+    `(let ,(column-bindings vars row) ,@body)))
+
+(defun column-bindings (vars row)
+  (loop for v in vars collect `(,v (column-value ,row ,(as-keyword v)))))
+
+(defun as-keyword (symbol)
+  (intern (symbol-name symbol) :keyword))
+
+(defun table-size (table)
+  (length (rows table)))
+
+(defun nth-row (n table)
+  (aref (rows table) n))
+
+(defun delete-rows (&key from where)
+  (loop
+    with rows = (rows from)
+    with store-idx = 0
+    for read-idx from 0
+    for row across rows
+    do (setf (aref rows read-idx) nil)
+    unless (funcall where row) do
+      (setf (aref rows store-idx) row)
+      (incf store-idx)
+    finally (setf (fill-pointer rows) store-idx)))
+
+(defun delete-all-rows (table)
+  (setf (rows table) (make-rows *default-table-size*)))
+
+(defun sort-rows (table &rest column-names)
+  (setf (rows table) (sort (rows table) (row-comparator column-names (schema table))))
+  table)
+
+(defun shuffle-table (table)
+  (nshuffle-vector (rows table))
+  table)
+
+(defun random-selection (table n)
+  (make-instance
+   'table
+   :schema (schema table)
+   :rows (nshuffle-vector (random-sample (rows table) n))))
+
+(defun random-sample (vector n)
+  "Based on Algorithm S from Knuth. TAOCP, vol. 2. p. 142"
+  (loop with selected = (make-array n :fill-pointer 0)
+        for idx from 0
+        do
+           (loop
+             with to-select = (- n (length selected))
+             for remaining = (- (length vector) idx)
+             while (>= (* remaining (random 1.0)) to-select)
+             do (incf idx))
+           (vector-push (aref vector idx) selected)
+        when (= (length selected) n) return selected))
+
+
